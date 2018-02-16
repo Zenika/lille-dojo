8e4c1de4cd62f0242a3c366785d2864a5a8d1bae
zenika <zenika@localhost.localdomain>
2018-02-16 13:09:53 +0100

    Feat: qdd string interpolation

--- number-in-words/src/main/java/NumberInWords.kt
+++ number-in-words/src/main/java/NumberInWords.kt
@@ -2,12 +2,10 @@ package main.java
 
 fun convert(number: Double): String {
     val numbers = hashMapOf(1 to "one", 2 to "two", 3 to "three", 4 to "four", 5 to "five", 6 to "six", 10 to "ten", 20 to "twenty")
-    val dozen = number / 10
-    val unit = number.toInt() % 10
-    val dozenStr = numbers.getOrDefault(dozen*10, "")
-    val dozens = hashMapOf(2 to "twenty")
+    val digit = number.toInt() % 10
+    val dozen = number - digit
 
-    return numbers.getOrDefault(number.toInt(), "") + getSuffix(number)
+    return "${numbers.getOrDefault(dozen.toInt(), "")} ${numbers.getOrDefault(digit, "")} ${getSuffix(number)}"
 }
 
-fun getSuffix(number: Double) = " dollar".takeIf { number <= 1 } ?: " dollars"
+fun getSuffix(number: Double) = "dollar".takeIf { number <= 1 } ?: "dollars"
