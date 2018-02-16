4c46f251ef249cbbd6b48e3c44267e566059a643
zenika <zenika@localhost.localdomain>
2018-02-16 12:56:28 +0100

    Next iteration

--- number-in-words/src/main/java/NumberInWords.kt
+++ number-in-words/src/main/java/NumberInWords.kt
@@ -1,10 +1,8 @@
 package main.java
 
-fun convert(number: Double): String? {
-    val numbers = hashMapOf(1 to "one", 2 to "two", 3 to "three", 4 to "four", 5 to "five", 6 to "six")
-    return numbers[number.toInt()] + getSuffix(number)
+fun convert(number: Double): String {
+    val numbers = hashMapOf(1 to "one", 2 to "two", 3 to "three", 4 to "four", 5 to "five", 6 to "six", 10 to "ten", 20 to "twenty")
+    return numbers.getOrDefault(number.toInt(), "") + getSuffix(number)
 }
 
-fun getSuffix(number: Double): String {
-    return " dollar".takeIf { number <= 1 } ?: " dollars";
-}
+fun getSuffix(number: Double) = " dollar".takeIf { number <= 1 } ?: " dollars"
--- number-in-words/src/test/java/NumberInWordsTest.kt
+++ number-in-words/src/test/java/NumberInWordsTest.kt
@@ -11,13 +11,26 @@ class NumberInWordsTest {
 
     @Test
     fun oneDollar() {
-        Assertions.assertThat(convert(1.0)).isEqualTo("one dollar");
+        Assertions.assertThat(convert(1.0)).isEqualTo("one dollar")
     }
 
     @Test
     fun twoDollars() {
-        Assertions.assertThat(convert(2.0)).isEqualTo("two dollars");
+        Assertions.assertThat(convert(2.0)).isEqualTo("two dollars")
     }
 
+    @Test
+    fun `assert 10 dollars`() {
+        Assertions.assertThat(convert(10.0)).isEqualTo("ten dollars")
+    }
 
+    @Test
+    fun `assert 20 dollars`() {
+        Assertions.assertThat(convert(20.0)).isEqualTo("twenty dollars")
+    }
+
+    @Test
+    fun `assert 21 dollars`() {
+       // Assertions.assertThat(convert(21.0)).isEqualTo("twenty one dollars")
+    }
 }
