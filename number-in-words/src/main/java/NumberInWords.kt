0ee3d0104cd30715fddc8964391105873a0ec1d4
antoine cordier <com.acordier@gmail.com>
2018-02-18 17:45:42 +0100

    number-in-words: pretty bad solution

--- number-in-words/src/main/java/NumberInWords.kt
+++ number-in-words/src/main/java/NumberInWords.kt
@@ -1,36 +1,91 @@
 package main.java
 
-val numbers = hashMapOf(1 to "one", 2 to "two", 3 to "three", 4 to "four", 5 to "five", 6 to "six", 10 to "ten", 20 to "twenty")
+import java.math.BigDecimal
 
-fun convertIntPart(number: Int, suffix: (n: Double) -> String): String  {
-    val digit = number % 10
-    val dozen = number - digit
+val numbers = hashMapOf(
+        0 to "zero",
+        1 to "one",
+        2 to "two",
+        3 to "three",
+        4 to "four",
+        5 to "five",
+        6 to "six",
+        7 to "seven",
+        8 to "eight",
+        9 to "nine",
+        10 to "ten",
+        11 to "eleven",
+        12 to "twelve",
+        13 to "thirteen",
+        14 to "fourteen",
+        15 to "fifteen",
+        16 to "sixteen",
+        17 to "seventeen",
+        18 to "eighteen",
+        19 to "nineteen",
+        20 to "twenty",
+        30 to "thirty",
+        40 to "forty",
+        50 to "fifty",
+        60 to "sixty",
+        70 to "seventy",
+        80 to "eighty",
+        90 to "ninety"
+)
 
-    var returned = mutableListOf<String>()
+fun getDecimals(number: Double) = BigDecimal.valueOf(number).divideAndRemainder(BigDecimal.ONE)[1].toFloat() * 100
 
-    if (dozen > 0) returned.add(numbers.getOrDefault(dozen, ""))
+fun pluralize(token: String) = { n: Int -> token.takeIf { n <= 1 } ?: "${token}s" }
 
-    if (digit > 0) returned.add(numbers.getOrDefault(digit, ""))
+fun hundredWords(digits: List<Int>) = digits
+        .zip(listOf("hundred and ", "", ""))
+        .map { "${numbers[it.first]} ${it.second}" }
+        .filter { !it.matches(Regex("zero.*")) }
+        .fold(String()) { acc, str -> acc + str }
+        .replace(Regex("and.$"), "")
+        .trim()
 
-    return "${returned.reduce { acc, cur -> acc.plus(" ").plus(cur)} } ${suffix(number.toDouble())}"
+fun numberPartition(number: Int): List<List<Int>> {
+    val parts = mutableListOf<List<Int>>()
+    var remainder = number
+    do {
+        parts.add(hundredPartition(remainder % 1000))
+        remainder -= remainder % 1000
+        remainder /= 1000
+    } while (remainder > 0)
+    return parts
 }
 
-fun convertDecimalPart(number: Double): Int {
-    return number.toString().split(".").getOrElse(1){"0"}.toInt() * 10
+fun hundredPartition(hundred: Int): List<Int> {
+    val tens = hundred % 100
+    val units = (tens % 10).takeIf { tens > 20 && tens % 10 != 0 } ?: 0
+    val hundreds = (hundred - tens) / 100
+    return listOf(hundreds, tens - units, units)
 }
 
 fun convert(number: Double): String {
-    val intPart = convertIntPart(number.toInt(), getSuffix("dollar"))
-
-    val decimalDigits = convertDecimalPart(number)
-
-    print(decimalDigits)
-    if (decimalDigits == 0) {
-        return intPart
+    val cents = getDecimals(number).toInt()
+    val dollars = number.toInt()
+    val multipliers = listOf("", "thousand,", "million,", "billion,")
+    val partition = numberPartition(dollars)
+    var words = partition.map { hundredWords(it) }
+            .zip(multipliers)
+            .filterNot { it.first.isEmpty() }
+            .reversed()
+            .map { "${it.first} ${it.second} " }
+            .fold(String()) { acc, str -> acc + str }
+            .replace(Regex(",.$"), "")
+            .trim()
+    if (dollars > 0) {
+        words += pluralize(" dollar")(dollars)
     }
-
-    return intPart + " and " + convertIntPart(decimalDigits, getSuffix("cent"))
+    if (dollars > 0 && cents > 0) {
+        words += " and "
+    }
+    if (cents > 0) {
+        words += hundredWords(hundredPartition(cents)) + pluralize(" cent")(cents)
+    }
+    return words
 }
 
-fun getSuffix(suffix: String) = { n:Double -> suffix.takeIf { n <= 1.0 } ?: "${suffix}s" }
 
--- number-in-words/src/test/java/NumberInWordsTest.kt
+++ number-in-words/src/test/java/NumberInWordsTest.kt
@@ -1,5 +1,6 @@
 import main.java.convert
 import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.*
 import org.junit.Test
 import kotlin.test.fail
 
@@ -10,35 +11,69 @@ class NumberInWordsTest {
 
 
     @Test
-    fun oneDollar() {
-        Assertions.assertThat(convert(1.0)).isEqualTo("one dollar")
+    fun `one dollar`() {
+        assertThat(convert(1.0)).isEqualTo("one dollar")
     }
 
     @Test
-    fun twoDollars() {
-        Assertions.assertThat(convert(2.0)).isEqualTo("two dollars")
+    fun `assert two dollars`() {
+        assertThat(convert(2.0)).isEqualTo("two dollars")
     }
 
     @Test
-    fun `assert 10 dollars`() {
-        Assertions.assertThat(convert(10.0)).isEqualTo("ten dollars")
+    fun `assert ten dollars`() {
+        assertThat(convert(10.0)).isEqualTo("ten dollars")
     }
 
     @Test
-    fun `assert 20 dollars`() {
-        Assertions.assertThat(convert(20.0)).isEqualTo("twenty dollars")
+    fun `assert twenty dollars`() {
+        assertThat(convert(20.0)).isEqualTo("twenty dollars")
     }
 
     @Test
-    fun `assert 21 dollars`() {
-       Assertions.assertThat(convert(21.0)).isEqualTo("twenty one dollars")
+    fun `assert twenty one dollars`() {
+       assertThat(convert(21.0)).isEqualTo("twenty one dollars")
     }
 
 
     @Test
-    fun `assert 21 dollars and 20 cents`() {
-        Assertions.assertThat(convert(21.2)).isEqualTo("twenty one dollars and twenty cents")
+    fun `assert twenty one dollars and twenty cents`() {
+        assertThat(convert(21.2)).isEqualTo("twenty one dollars and twenty cents")
     }
 
+    @Test
+    fun `assert eleven dollars and twenty cents`() {
+        assertThat(convert(11.2)).isEqualTo("eleven dollars and twenty cents")
+    }
+
+    @Test
+    fun `assert one hundred dollars and forty five cents`() {
+        assertThat(convert(100.45)).isEqualTo("one hundred dollars and forty five cents")
+    }
+
+    @Test
+    fun `four hundred and fifty six dollars and twenty three cents`() {
+        assertThat(convert(456.23)).isEqualTo("four hundred and fifty six dollars and twenty three cents")
+    }
+
+    @Test
+    fun `assert one thousand dollars`() {
+        assertThat(convert(1E3)).isEqualTo("one thousand dollars")
+    }
+
+    @Test
+    fun `assert one hundred thousand dollars`() {
+        assertThat(convert(1E5)).isEqualTo("one hundred thousand dollars")
+    }
+
+    @Test
+    fun `assert one million dollars`() {
+        assertThat(convert(1E6)).isEqualTo("one million dollars")
+    }
+
+    @Test
+    fun `assert one billion dollars`() {
+        assertThat(convert(1E9)).isEqualTo("one billion dollars")
+    }
 
 }
